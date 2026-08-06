888403223f17b2a46bbbf3a9d759a947e2c09340
anthonybell1419-art <anthonybell1419@gmail.com>
2026-08-05 20:21:58 -0400

    Se agrego musica en el menu

--- Laberinto/src/Menu.java
+++ Laberinto/src/Menu.java
@@ -8,15 +8,24 @@ import javafx.scene.text.*;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 
-
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class Menu {
+    private static MediaPlayer musica;
     public static Scene crear(Stage stage){
         VBox root = new VBox(22);
+
+        Media sonido = new Media(Menu.class.getResource("/Musica/MenuMusica.mp3").toExternalForm());
+        musica = new MediaPlayer(sonido);
+        musica.setCycleCount(MediaPlayer.INDEFINITE);
+        musica.setVolume(0.5);
+        musica.play();
+
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(40));
         root.setStyle(
-            "-fx-background-image: url('/imagenes/Menu.png');" +
+            "-fx-background-image: url('/Imagenes/Menu.png');" +
             "-fx-background-size: cover;" +
             "-fx-background-position: center center;" +
             "-fx-background-repeat: no-repeat;");
@@ -82,7 +91,24 @@ public class Menu {
     // btn.setOnAction(event -> stage.setScene(Juego.crear(stage, dificultad)));
     btnBox.getChildren().add(btn);
 }
-        root.getChildren().addAll( btnBox);
-        return new Scene(root, 420, 520);
+        Button btnMusica = new Button("🔊 Música");
+        btnMusica.setOnAction(e -> {
+
+        if(musica.getStatus() == MediaPlayer.Status.PLAYING){
+            musica.pause();
+            btnMusica.setText("🔇 Música");
+        }else{
+            musica.play();
+            btnMusica.setText("🔊 Música");
+    }});
+
+        btnMusica.setStyle(
+            "-fx-background-color:#7B2CBF;" +
+            "-fx-text-fill:white;" +
+            "-fx-background-radius:8;" +
+            "-fx-cursor:hand;");
+
+root.getChildren().addAll(btnBox, btnMusica);
+return new Scene(root, 420, 520);
     }
 }
